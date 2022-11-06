67ab3a1bf1ebcc15d1b1c0238d968ac9ab5dd7d4
Gabriel Sanchez <gabrielpelota.8@gmail.com>
2022-11-06 09:05:01 -0500

    first commit

---
+++ src/components/Interface.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Interface = () => {
+  return (
+    <div className='container interface'>
+        <form action="">
+            
+        </form>
+    </div>
+  )
+}
+
+export default Interface
